b7c1c8578e9d15b8a987f4b5936a5918f5e9cedb
RobertaKelly <robatucadas@hotmail.com>
2020-12-04 13:30:33 -0300

    ajust/Readme

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import data from './data/rickandmorty/rickandmorty.js';
-import { filterAllInfo, orderAZ, orderZA, elements, searchNames, statistics} from './data.js';
+import {
+  filterAllInfo,
+  orderAZ,
+  orderZA,
+  elements,
+  searchNames,
+  statistics
+} from './data.js';
 const getInfo = data.results
 const showStatistics = document.getElementById("printCalculaations");
+
 function charactersInfo(list) {
-    let insertHtml ="";
-    insertHtml += list.map((item) =>
+  let insertHtml = "";
+  insertHtml += list.map((item) =>
     `<div class="cards">
      <img id="image" src="${item.image}" alt="${item.name}">
      <div id="name"> <strong>${item.name}</strong></div>
@@ -12,75 +20,78 @@ function charactersInfo(list) {
      <br><strong>Status:</strong>${item.status}
      <br><strong>Species:</strong>${item.species}</p>
     </div>`).join("");
-    document.getElementById("showCards").innerHTML = insertHtml;
+  document.getElementById("showCards").innerHTML = insertHtml;
 }
 charactersInfo(getInfo);
 //filter: Gender
 const genderSelector = document.getElementById("Gêneros-quantidade");
 genderSelector.addEventListener("change", genderFilter);
-function genderFilter(){
-    if(genderSelector.value === "Femininos"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "gender", "Female")}`
-        return charactersInfo(filterAllInfo(elements, "gender", "Female"));
-    } else if(genderSelector.value === "Masculinos"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "gender", "Male")}`
-        return charactersInfo(filterAllInfo(elements, "gender", "Male"));
-    } else if(genderSelector.value === "Sem-gênero"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "gender", "unknown")}`
-        return charactersInfo(filterAllInfo(elements, "gender", "unknown"));
-    } 
+
+function genderFilter() {
+  if (genderSelector.value === "Femininos") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "gender", "Female")}`
+    return charactersInfo(filterAllInfo(elements, "gender", "Female"));
+  } else if (genderSelector.value === "Masculinos") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "gender", "Male")}`
+    return charactersInfo(filterAllInfo(elements, "gender", "Male"));
+  } else if (genderSelector.value === "Sem-gênero") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "gender", "unknown")}`
+    return charactersInfo(filterAllInfo(elements, "gender", "unknown"));
+  }
 }
 //filter: Status
 const statusSelector = document.getElementById("Caracteristicas-quantidade");
 statusSelector.addEventListener("change", statusFilter);
-function statusFilter(){
-    if(statusSelector.value === "Vivos"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "status", "Alive")}`
-        return charactersInfo(filterAllInfo(elements, "status", "Alive"))
-    } else if(statusSelector.value === "Mortos"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "status", "Dead")}`
-        return charactersInfo(filterAllInfo(elements, "status", "Dead"))
-    } else if(statusSelector.value === "Desconhecidos"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "status", "unknown")}`
-        return charactersInfo(filterAllInfo(elements, "status", "unknown"))
-    }
+
+function statusFilter() {
+  if (statusSelector.value === "Vivos") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "status", "Alive")}`
+    return charactersInfo(filterAllInfo(elements, "status", "Alive"))
+  } else if (statusSelector.value === "Mortos") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "status", "Dead")}`
+    return charactersInfo(filterAllInfo(elements, "status", "Dead"))
+  } else if (statusSelector.value === "Desconhecidos") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "status", "unknown")}`
+    return charactersInfo(filterAllInfo(elements, "status", "unknown"))
+  }
 }
 //filter: Species
 const speciesSelector = document.getElementById("Especies-quantidade")
 speciesSelector.addEventListener("change", speciesFilter)
-function speciesFilter(){
-    if(speciesSelector.value === "Humanos"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Human")}`
-        return charactersInfo(filterAllInfo(elements, "species", "Human"))
-    } else if(speciesSelector.value === "Humanoids"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Humanoid")}`
-        return charactersInfo(filterAllInfo(elements, "species", "Humanoid"))
-    } else if (speciesSelector.value === "Aliens"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Alien")}`
-        return charactersInfo(filterAllInfo(elements, "species", "Alien"))
-    } else if (speciesSelector.value === "Animais"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Animal")}`
-        return charactersInfo(filterAllInfo(elements, "species", "Animal"))
-    } else if(speciesSelector.value === "Cronenberg"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Cronenberg")}`
-        return charactersInfo(filterAllInfo(elements, "species", "Cronenberg"))
-    } else if(speciesSelector.value === "Robot"){
-        showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Robot")}`
-        return charactersInfo(filterAllInfo(elements, "species", "Robot"))
-    }
+
+function speciesFilter() {
+  if (speciesSelector.value === "Humanos") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Human")}`
+    return charactersInfo(filterAllInfo(elements, "species", "Human"))
+  } else if (speciesSelector.value === "Humanoids") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Humanoid")}`
+    return charactersInfo(filterAllInfo(elements, "species", "Humanoid"))
+  } else if (speciesSelector.value === "Aliens") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Alien")}`
+    return charactersInfo(filterAllInfo(elements, "species", "Alien"))
+  } else if (speciesSelector.value === "Animais") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Animal")}`
+    return charactersInfo(filterAllInfo(elements, "species", "Animal"))
+  } else if (speciesSelector.value === "Cronenberg") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Cronenberg")}`
+    return charactersInfo(filterAllInfo(elements, "species", "Cronenberg"))
+  } else if (speciesSelector.value === "Robot") {
+    showStatistics.innerHTML = `O número de personagens dessa categoria é ${statistics(elements, "species", "Robot")}`
+    return charactersInfo(filterAllInfo(elements, "species", "Robot"))
+  }
 }
 //filter: order AZ/ZA
 const sortSelector = document.getElementById("sort-characters");
 sortSelector.addEventListener("change", sortBy);
-function sortBy(){
-    if(sortSelector.value === "sortAZ"){
-        return charactersInfo(orderAZ(elements))
-    } else if(sortSelector.value === "sortZA"){
-        return charactersInfo(orderZA(elements))
-    }
+
+function sortBy() {
+  if (sortSelector.value === "sortAZ") {
+    return charactersInfo(orderAZ(elements))
+  } else if (sortSelector.value === "sortZA") {
+    return charactersInfo(orderZA(elements))
+  }
 }
 //function search
 const searchInput = document.getElementById("nameSearch");
 searchInput.addEventListener("keyup", () => charactersInfo(searchNames(searchInput.value.toUpperCase(), elements)));
 //calculations
-
